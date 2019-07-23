44c17af020107f99e27af1b7050ea289ece88ab2
genaralskar <genaralskar1@gmail.com>
2019-07-23 14:54:57 -0600

    add hex grid project

---
+++ Unity/Hex Grid Map/Assets/HexCoordinates.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public struct HexCoordinates
+{
+    [SerializeField]
+    private int x, z;
+    
+    public int X
+    {
+        get { return x; }
+    }
+
+    public int Z
+    {
+        get { return z; }
+    }
+
+    public HexCoordinates(int x, int z)
+    {
+        this.x = x;
+        this.z = z;
+    }
+
+    
+
+    public static HexCoordinates FromOffsetCoordinates(int x, int z)
+    {
+        return new HexCoordinates(x - z / 2, z);
+    }
+
+    public int Y
+    {
+        get { return -X - Z; }
+    }
+    
+    public override string ToString()
+    {
+        return "(" + X.ToString() + ", " + Y.ToString() + ", " + Z.ToString() + ")";
+    }
+    
+    public string ToStringOnSeparateLines()
+    {
+        return X.ToString() + "\n" + Y.ToString() + "\n" + Z.ToString();
+    }
+
+    public static HexCoordinates FromPosition(Vector3 position)
+    {
+        float x = position.x / (HexMetrics.innerRadius * 2f);
+        float y = -x;
+        
+        float offset = position.z / (HexMetrics.outerRadius * 3f);
+        x -= offset;
+        y -= offset;
+
+        int iX = Mathf.RoundToInt(x);
+        int iY = Mathf.RoundToInt(y);
+        int iZ = Mathf.RoundToInt(-x - y);
+
+        if (iX + iY + iZ != 0)
+        {
+            //fixes some rounding errors
+            //all 3 coordinate of a hex system should add to 0
+            //dX, dY, dZ compares the int version with the non int version
+            //then checks which number is getting rounded wrong and fixes it(not entierly sure how here)
+            
+            float dX = Mathf.Abs(x - iX);
+            float dY = Mathf.Abs(y - iY);
+            float dZ = Mathf.Abs(-x -y - iZ);
+
+            if (dX > dY && dX > dZ)
+            {
+                iX = -iY - iZ;
+            }
+            else if (dZ > dY)
+            {
+                iZ = -iX - iY;
+            }
+        }
+        
+        return new HexCoordinates(iX, iZ);
+    }
+}
---
+++ Unity/Hex Grid Map/Assets/HexGrid.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HexGrid : MonoBehaviour
+{
+    public int width = 6;
+    public int height = 6;
+
+    public HexCell cellPrefab;
+
+    private HexCell[] cells;
+
+    public Text cellLabelPrefab;
+    private Canvas gridCanvas;
+
+    private HexMesh hexMesh;
+
+    public Color defualtColor = Color.white;
+
+    private void Awake()
+    {
+        gridCanvas = GetComponentInChildren<Canvas>();
+        hexMesh = GetComponentInChildren<HexMesh>();
+        
+        cells = new HexCell[height * width];
+
+        for (int z = 0, i = 0; z < height; z++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                CreateCell(x, z, i++);
+            }
+        }
+    }
+
+    private void CreateCell(int x, int z, int i)
+    {
+        Vector3 position;
+        position.x = (x + z * 0.5f - z/2) * (HexMetrics.innerRadius * 2f);
+        position.y = 0f;
+        position.z = z * (HexMetrics.outerRadius * 1.5f);
+
+        HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
+        cell.transform.SetParent(transform, false);
+        cell.transform.localPosition = position;
+        cell.coordinates = HexCoordinates.FromOffsetCoordinates(x, z);
+        cell.color = defualtColor;
+
+
+        Text label = Instantiate(cellLabelPrefab);
+        label.rectTransform.SetParent(gridCanvas.transform, false);
+        label.rectTransform.anchoredPosition = new Vector3(position.x, position.z);
+        label.text = cell.coordinates.ToStringOnSeparateLines();
+    }
+    
+    private void Start()
+    {
+        hexMesh.Triangulate(cells);
+    }
+
+    public void ColorCell(Vector3 position, Color color)
+    {
+        position = transform.InverseTransformPoint(position);
+        HexCoordinates coordinates = HexCoordinates.FromPosition(position);
+        int index = coordinates.X + coordinates.Z * width + coordinates.Z / 2;
+        HexCell cell = cells[index];
+        cell.color = color;
+        hexMesh.Triangulate(cells);
+    }
+}
